7eebff2b030e02530a06cbe179f767c6461cdee1
thanhenc <thanhenc@gmail.com>
2025-04-20 16:49:06 +0700

    feat: tools - current price, price history and market index

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,46 @@ export class MyMCP extends McpAgent {
 	});
 
 	async init() {
+		// Demo tools
 		this.server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
 			content: [{ type: "text", text: String(a + b) }],
 		}));
+
+		// Securities tool
+		// 1. Get current price of a stock or a list of stocks
+		// Implementation:
+		// - Call API https://price.vixs.vn/datafeed/instruments?symbols=ACB,VCB
+		// - Return the price of the stock
+		this.server.tool("get-stock-price", { symbols: z.array(z.string()) }, async ({ symbols }) => {
+			const response = await fetch(`https://price.vixs.vn/datafeed/instruments?symbols=${symbols.join(",")}`);
+			const data = await response.json();
+			return {
+				content: [{ type: "text", text: JSON.stringify(data) }],
+			};
+		});
+
+		// 2. Get Market Index
+		// Implementation:
+		// - Call API https://api2.simplize.vn/api/company/se/market?type=index
+		// - Return the price of the index
+		this.server.tool("get-market-index", {}, async () => {
+			const response = await fetch(`https://api2.simplize.vn/api/company/se/market?type=index`);
+			const data = await response.json();
+			return {
+				content: [{ type: "text", text: JSON.stringify(data) }],
+			};
+		});
+		
+		// 3. Get Price history of a stock
+		// Implementation:
+		// - Call API https://price.vixs.vn/fiin/Price/GetHistory?fromDate=01/01/2025&limitRow=500&nextIndex=0&symbol=ACB&toDate=13/03/2025
+		this.server.tool("get-stock-price-history", { symbol: z.string(), fromDate: z.string(), toDate: z.string() }, async ({ symbol, fromDate, toDate }) => {
+			const response = await fetch(`https://price.vixs.vn/fiin/Price/GetHistory?fromDate=${fromDate}&limitRow=1000&nextIndex=0&symbol=${symbol}&toDate=${toDate}`);
+			const data = await response.json();
+			return {
+				content: [{ type: "text", text: JSON.stringify(data) }],
+			};
+		});
 	}
 }
 
